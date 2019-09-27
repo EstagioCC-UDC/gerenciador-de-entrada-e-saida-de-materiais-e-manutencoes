e0c0f7d7ffc2962a69bcf96dd789d78248a1400f
Matheus Lichtnow <matheusrobert8@gmail.com>
2019-09-27 18:36:14 -0300

    Added axios interceptor for error handling

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,20 @@ import { getTokens } from './userInfo';
 
 const token = getTokens().accessToken;
 
-export default axios.create({
+const api = axios.create({
   baseURL: env[process.env.NODE_ENV].backendUrl,
   headers: { Authorization: `Bearer ${token}` },
 });
+
+api.interceptors.response.use(
+  response => {
+    return Promise.resolve(response);
+  },
+  error => {
+    return error.response.data.error
+      ? Promise.reject(error.response.data.error)
+      : Promise.reject('Erro interno do servidor: 500');
+  }
+);
+
+export default api;
